e4a65a5ed3ee250083ad94a91d3dd51436d1340f
Emmi Lindsey <emmilindsey@gmail.com>
2023-11-22 11:44:03 -0700

    completed project - odd environment errors

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,12 +1,27 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ categories, onBtnClick, currentCategory, setCurrentCategory }) {
+
+  
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {categories.map((category) => {
+        return <button 
+        key={category} 
+        className={category === currentCategory ? "selected" : ""} 
+        onClick={(() => onBtnClick(category))}>
+        {category}
+        </button> 
+        
+      }) 
+        
+        }
     </div>
-  );
+
+  )
+    
 }
 
 export default CategoryFilter;
+
--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTaskForm() {
+
+function NewTaskForm({ categories, onTaskFormSubmit }) {
+  const [newDetails, setNewDetails] = useState("")
+  const [newCategory, setNewCategory] = useState("Code")
+
+
+ const newCategories = categories.filter((each) => {
+  return each !== "All"
+ })
+
+
+function handleDetailsChange(e) {
+  setNewDetails(e.target.value)
+}
+
+function handleCategoryChange(e) {
+  setNewCategory(e.target.value)
+}
+
+ function handleSubmit(e) {
+  e.preventDefault()
+
+  const newObj = {
+    category: "newCategory",
+    text: "newDetails"
+  }
+
+onTaskFormSubmit(newObj)
+
+setNewDetails("")
+setNewCategory("")
+
+    
+ }
+
+
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={handleSubmit}>
       <label>
         Details
-        <input type="text" name="text" />
+        <input type="text" name="text" onChange={handleDetailsChange} value={newDetails}/>
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select name="category" onChane={handleCategoryChange}>
+         {newCategories.map((each) => {
+          return <option key={each}>{each}</option>
+         })}
         </select>
       </label>
       <input type="submit" value="Add task" />
